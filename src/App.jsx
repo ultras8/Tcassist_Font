b5a2ff238952cef0c679c6a398ca00a8cb7bf593
Ploychomphu Tulsuk <ploychomphu.ths@gmail.com>
2025-12-25 22:45:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import Home from './pages/Home'
+import Admission from './pages/admission'
+
+function App() {
+  return (
+    <div className="bg-[#ADD6F2] min-h-screen">
+      <Admission />
+      {/* <Home /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/admission.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Admission() {
+  const [score, setScore] = useState('')
+
+  return (
+    <div className="bg-[#ADD6F2] flex flex-col items-center justify-center min-h-screen">
+      {/* 🧑‍💻 Banner / Navbar */}
+
+    </div>
+  )
+}
+
+export default Admission
